3feedea47ece143b2e2b6b5a61b8e2cbcb466cf6
J-Otanga <furahaotanga@gmail.com>
2023-12-20 12:39:17 +0300

    added comments

--- sript.js
+++ sript.js
@@ -4,8 +4,10 @@ function addTo(){
     const item = document.createElement("li");
     const list =document.getElementById("todo-container");
     item.innerText = toInput.value;
-    list.appendChild(item);
+    list.appendChild(item);// adds item to the list
+
     toInput.value = "";
+    // add line-through to check an item off the list
     item.addEventListener("click",() => {
         if (item.style.textDecoration === 'none') {
             item.style.textDecoration = 'line-through'
